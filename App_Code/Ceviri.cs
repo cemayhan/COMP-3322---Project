2099e919f9557c156b3ca9e539851c864f16d4a6
cemayhan <cayhan@harmonytx.org>
2017-05-07 19:29:09 -0500

    Add files via upload

---
+++ App_Code/Ceviri.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Net;
+
+/// <summary>
+/// This program is for translating words into English or vice versa.
+/// </summary>
+public class Ceviri
+{
+    public string Translate(string sourceText, string sourceLanguage, string targetLanguage)
+
+    {
+        
+        string translation = string.Empty;
+
+        try
+        {
+            // Download translation
+            string url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+                                        sourceLanguage,
+                                        targetLanguage,
+                                        HttpUtility.UrlEncode(sourceText));
+            string outputFile = Path.GetTempFileName();
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+                wc.DownloadFile(url, outputFile);
+            }
+
+            // Get translated text
+            if (File.Exists(outputFile))
+            {
+
+                // Get phrase collection
+                string text = File.ReadAllText(outputFile);
+                int index = text.IndexOf(string.Format(",,\"{0}\"", sourceLanguage));
+                if (index == -1)
+                {
+                    // Translation of single word
+                    int startQuote = text.IndexOf('\"');
+                    if (startQuote != -1)
+                    {
+                        int endQuote = text.IndexOf('\"', startQuote + 1);
+                        if (endQuote != -1)
+                        {
+                            translation = text.Substring(startQuote + 1, endQuote - startQuote - 1);
+                        }
+                    }
+                }
+                else
+                {
+                    // Translation of phrase
+                    text = text.Substring(0, index);
+                    text = text.Replace("],[", ",");
+                    text = text.Replace("]", string.Empty);
+                    text = text.Replace("[", string.Empty);
+                    text = text.Replace("\",\"", "\"");
+
+                    // Get translated phrases
+                    string[] phrases = text.Split(new[] { '\"' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 0; (i < phrases.Count()); i += 2)
+                    {
+                        string translatedPhrase = phrases[i];
+                        if (translatedPhrase.StartsWith(",,"))
+                        {
+                            i--;
+                            continue;
+                        }
+                        translation += translatedPhrase + "  ";
+                    }
+                }
+
+                // Fix up translation
+                translation = translation.Trim();
+                translation = translation.Replace(" ?", "?");
+                translation = translation.Replace(" !", "!");
+                translation = translation.Replace(" ,", ",");
+                translation = translation.Replace(" .", ".");
+                translation = translation.Replace(" ;", ";");
+
+                // And translation speech URL
+                string url2 = string.Format("https://translate.googleapis.com/translate_tts?ie=UTF-8&q={0}&tl={1}&total=1&idx=0&textlen={2}&client=gtx", HttpUtility.UrlEncode(translation), targetLanguage, translation.Length);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        
+        return translation;
+    }
+}
+
+
---
+++ App_Code/hackcheck.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+/// <summary>
+/// Summary description for hackcheck
+/// </summary>
+public class hackcheck
+{
+    public Boolean Hacker()
+    {
+        
+
+        HttpCookie myCookie = new HttpCookie("speaknlearn.com");
+        myCookie = HttpContext.Current.Request.Cookies.Get("speaknlearn.com");
+        string user = null;
+        string ssid = null;
+        if (myCookie == null)
+        {
+            return false;
+        }
+        else
+        {
+            user = myCookie["username"];
+            ssid = myCookie["sessionid"];
+        }
+
+        ssid = EncryptDecrypt.Decrypt(ssid, true);
+
+        var passcheck = new Regex("('(''|[^'])*')|(;)|(\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b)");
+
+        if (passcheck.IsMatch(user) || passcheck.IsMatch(ssid))
+        {
+            return false;
+        }
+        else
+        {
+
+            string config = ConfigurationManager.ConnectionStrings["data2ConnectionString"].ConnectionString;
+            MySqlConnection conn = new MySqlConnection(config);
+
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = config;
+
+            string giris = "SELECT sifre FROM kullanici WHERE kullaniciadi = @Kullaniciadi";
+            MySqlCommand cmd = new MySqlCommand(giris, conn);
+            cmd.Parameters.AddWithValue("@Kullaniciadi", user);
+            conn.Open();
+
+            string decryptedpass = EncryptDecrypt.Decrypt(cmd.ExecuteScalar().ToString(), true);
+
+            conn.Close();
+
+            if (ssid != decryptedpass)
+            {
+                return false;
+            }
+            else
+            {
+                string test2;
+                var regexItem = new Regex("^[a-zA-Z0-9]*$");
+                test2 = HttpContext.Current.Request.QueryString["unit"];
+
+                if (test2 == null && regexItem.IsMatch(test2))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
